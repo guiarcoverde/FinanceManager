dc9d361e58dfa46f31b84a9cf0071ee8a19cbc31
guiarcoverde <gui.arcoverde21@gmail.com>
2024-05-17 15:08:14 -0300

    add: new interfaces

---
+++ src/FinanceManager.Domain/Repositories/Expenses/IExpenseReadOnlyRepository.cs
@@ -0,0 +1,8 @@
+﻿using FinanceManager.Domain.Entities;
+
+namespace FinanceManager.Domain.Repositories.Expenses;
+public interface IExpenseReadOnlyRepository
+{
+    Task<List<Expense>> GetAll();
+    Task<Expense?> GetById(long id);
+}
