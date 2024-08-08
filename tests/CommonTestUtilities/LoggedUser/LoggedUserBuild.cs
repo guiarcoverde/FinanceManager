4e0a4ba508a942f8307a1e7654c122c614f3a7cc
guiarcoverde <gui.arcoverde21@gmail.com>
2024-08-08 09:56:04 -0300

    Builder for tests

---
+++ tests/CommonTestUtilities/LoggedUser/LoggedUserBuild.cs
@@ -0,0 +1,16 @@
+﻿using FinanceManager.Domain.Entities;
+using FinanceManager.Domain.Services.LoggedUser;
+using Moq;
+
+namespace Common.TestUtilities.LoggedUser;
+
+public class LoggedUserBuild
+{
+    public static ILoggedUser Build(User user)
+    {
+        var mock = new Mock<ILoggedUser>();
+        mock.Setup(loggedUser => loggedUser.Get()).ReturnsAsync(user);
+
+        return mock.Object;
+    }
+}
