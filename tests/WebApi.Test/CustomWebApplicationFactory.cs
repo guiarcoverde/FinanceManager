20e497401d9e1c1bda316fc7ffb7487fd3bb6b99
guiarcoverde <gui.arcoverde21@gmail.com>
2024-08-08 09:54:38 -0300

    Quick refatoring on the CustomWebApplicationFactory.cs

--- tests/WebApi.Test/CustomWebApplicationFactory.cs
+++ tests/WebApi.Test/CustomWebApplicationFactory.cs
@@ -1,6 +1,7 @@
 ﻿using Common.TestUtilities.Entities;
 using FinanceManager.Domain.Entities;
 using FinanceManager.Domain.Security.Cryptography;
+using FinanceManager.Domain.Security.Tokens;
 using FinanceManager.Infrastructure.DataAccess;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
@@ -12,7 +13,9 @@ namespace WebApi.Test;
 public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
     private User _user;
+    private Expense _expense;
     private string _password;
+    private string _token;
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Test")
@@ -30,12 +33,25 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
                 var passwordEncryptor = scope.ServiceProvider.GetRequiredService<IPasswordEncryptor>();
 
                 StartDatabase(dbContext, passwordEncryptor);
+
+                var tokenGenerator = scope.ServiceProvider.GetRequiredService<IAccessTokenGenerator>();
+                _token = tokenGenerator.Generate(_user);
             });
     }
     public string GetName() => _user.Name;
     public string GetEmail() => _user.Email;
     public string GetPassword() => _password;
+    public string GetToken() => _token;
+    public long GetExpenseId() => _expense.Id;
     private void StartDatabase(FinanceManagerDbContext dbContext, IPasswordEncryptor passwordEncryptor)
+    {
+        AddUsers(dbContext, passwordEncryptor);
+        AddExpenses(dbContext, _user);
+
+        dbContext.SaveChanges();
+    }
+
+    private void AddUsers(FinanceManagerDbContext dbContext, IPasswordEncryptor passwordEncryptor)
     {
         _user = UserBuilder.Build();
         _password = _user.Password;
@@ -43,7 +59,13 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
         _user.Password = passwordEncryptor.Encrypt(_user.Password);
 
         dbContext.Users.Add(_user);
-
-        dbContext.SaveChanges();
     }
+
+    private void AddExpenses(FinanceManagerDbContext dbContext, User user)
+    {
+        _expense = ExpenseBuilder.Build(user);
+        dbContext.Expenses.Add(_expense);
+    }
+    
+    
 }
