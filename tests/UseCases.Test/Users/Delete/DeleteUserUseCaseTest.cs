ebd3e04611da937d341c6429b4de9fc6bb405b4b
Guilherme Cavalcanti <guilherme.cavalcanti@accenture.com>
2024-08-13 14:43:22 -0300

    Delete user use case tests

---
+++ tests/UseCases.Test/Users/Delete/DeleteUserUseCaseTest.cs
@@ -0,0 +1,32 @@
+﻿using Common.TestUtilities.Entities;
+using Common.TestUtilities.LoggedUser;
+using Common.TestUtilities.Repositories;
+using FinanceManager.Application.UseCases.Users.Delete;
+using FinanceManager.Domain.Entities;
+using FluentAssertions;
+
+namespace UseCases.Test.Users.Delete;
+
+public class DeleteUserUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var user = UserBuilder.Build();
+        var useCase = CreateUseCase(user);
+
+        var act = async () => await useCase.Execute();
+
+        await act.Should().NotThrowAsync();
+    }
+
+
+    private DeleteUserUseCase CreateUseCase(User user)
+    {
+        var repository = UserWriteOnlyRepositoryBuilder.Build();
+        var loggedUser = LoggedUserBuild.Build(user);
+        var unitOfWork = UnitOfWorkBuilder.Build();
+
+        return new DeleteUserUseCase(loggedUser, repository, unitOfWork);
+    }
+}
---
+++ tests/WebApi.Test/Users/Delete/DeleteUserTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using FluentAssertions;
+
+namespace WebApi.Test.Users.Delete;
+public class DeleteUserTest : FinanceManagerClassFixture
+{
+    private const string Method = "api/user";
+    private readonly string _token;
+
+    public DeleteUserTest(CustomWebApplicationFactory webApplicationFactory) : base(webApplicationFactory)
+    {
+        _token = webApplicationFactory.UserTeamMember.GetToken();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var result = await DoDelete(Method, _token);
+
+        result.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+}
+
